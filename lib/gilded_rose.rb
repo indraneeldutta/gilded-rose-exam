dbc05f2b2b0c325ae98989962562c65f529112d7
Indraneel Dutta <indraneel.dutta@hotmail.com>
2020-03-09 19:10:22 +0530

    conjured items degrade condition after sellin passed

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -38,9 +38,12 @@ class GildedRose
         if item.name != "Aged Brie"
           if item.name != "Backstage passes to a TAFKAL80ETC concert"
             if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
+              if item.name != "Sulfuras, Hand of Ragnaros" && item.name != "Conjured"
                 item.quality = item.quality - 1
-              end
+							end
+							if item.name == "Conjured"
+								item.quality = item.quality - 2
+							end
             end
           else
             item.quality = item.quality - item.quality
